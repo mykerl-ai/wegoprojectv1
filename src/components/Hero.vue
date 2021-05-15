ab61db1aeb5cccfc2e1bf354645a46cae325db85
mykerl-ai <michaelchigozie51@gmail.com>
2021-05-15 23:13:27 +0100

    fourth commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -20,7 +20,7 @@
      
     <div class="navbar md:hidden">
       <div class="dropdown1">
-    <button class="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" fill="#0080FF" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+    <button class="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg> 
     </button>
